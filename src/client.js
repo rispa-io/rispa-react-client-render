231cbdb7e474248c853d059e7ae702a36924a069
Vitaliy Matveev <vmatveev@itgroup.lc>
2018-12-13 11:34:07 +0300

    remove commented code

--- src/client.js
+++ src/client.js
@@ -3,12 +3,6 @@ import { renderToStaticMarkup } from 'react-dom/server'
 
 import Html from './Html'
 
-export default function (req, { assets, log }) {
-  log(assets)
-  // log('RENDER CLIENT', assets.compilation.assets)
-  // const headerAssets = Object.keys(assets.styles).map(style => (
-  //   <link href={assets.styles[style]} key={style} media='screen, projection' rel='stylesheet' type='text/css' charSet='UTF-8' />
-  // ))
+export default function (req, { assets }) {
   return renderToStaticMarkup(<Html assets={assets} />)
-  // return mergeTemplateWithAssets(template, { bodyAssets })
 }
